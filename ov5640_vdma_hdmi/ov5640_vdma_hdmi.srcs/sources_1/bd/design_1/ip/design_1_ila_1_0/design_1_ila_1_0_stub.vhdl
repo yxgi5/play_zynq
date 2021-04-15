c89e2548051d4cd662456e832179abf16a3f8b92
Andreas Zhang <denglitsch@gmail.com>
2021-04-15 11:25:37 +0800

    feat: ov5640 1024x768 ok

---
+++ ov5640_vdma_hdmi/ov5640_vdma_hdmi.srcs/sources_1/bd/design_1/ip/design_1_ila_1_0/design_1_ila_1_0_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
+-- Date        : Thu Apr 15 09:45:03 2021
+-- Host        : archlinux running 64-bit Arch Linux
+-- Command     : write_vhdl -force -mode synth_stub -rename_top design_1_ila_1_0 -prefix
+--               design_1_ila_1_0_ design_1_ila_0_1_stub.vhdl
+-- Design      : design_1_ila_0_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_ila_1_0 is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 23 downto 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe4 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe5 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe6 : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    probe7 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe8 : in STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end design_1_ila_1_0;
+
+architecture stub of design_1_ila_1_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[0:0],probe1[23:0],probe2[2:0],probe3[0:0],probe4[0:0],probe5[0:0],probe6[2:0],probe7[0:0],probe8[0:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2018.3";
+begin
+end;
