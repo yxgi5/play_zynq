b5b1fff082c8477d09db3f8036a40b03dda3a106
Andreas Zhang <denglitsch@gmail.com>
2021-04-29 02:37:48 +0800

    update: ov5640 raw 1080p

---
+++ ov5640/ov5640_raw_1080p_vdma_hdmi/sdk/src/config.h
@@ -0,0 +1,19 @@
+
+#ifndef SRC_CONFIG_H_
+#define SRC_CONFIG_H_
+
+#define P1080 1
+#define CAMERA 0
+
+#if P1080 == 1
+#define VIDEO_COLUMNS 1920
+#define VIDEO_ROWS 1080
+#else
+//#define VIDEO_COLUMNS 1280
+//#define VIDEO_ROWS 720
+#define VIDEO_COLUMNS 1024
+#define VIDEO_ROWS 768
+#endif
+
+
+#endif /* SRC_CONFIG_H_ */
