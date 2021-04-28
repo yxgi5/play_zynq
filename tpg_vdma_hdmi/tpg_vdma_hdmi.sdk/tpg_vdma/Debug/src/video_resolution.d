c65b13bda67a57168bbed191dddc869e72490341
Andreas Zhang <denglitsch@gmail.com>
2021-04-28 14:26:58 +0800

    update: tpg_vdma_hdmi tcl prj mode

--- tpg_vdma_hdmi/tpg_vdma_hdmi.sdk/tpg_vdma/Debug/src/video_resolution.d
+++
@@ -1,7 +0,0 @@
-src/video_resolution.o src/video_resolution.o: ../src/video_resolution.c \
- ../src/video_resolution.h \
- ../../tpg_vdma_bsp/ps7_cortexa9_0/include/xbasic_types.h
-
-../src/video_resolution.h:
-
-../../tpg_vdma_bsp/ps7_cortexa9_0/include/xbasic_types.h:
