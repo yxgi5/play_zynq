a0267991099584479215e2928022c5a45ccea5bb
Andreas Zhang <denglitsch@gmail.com>
2022-04-15 17:28:44 +0800

    update:

---
+++ dp_pl_video/compare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bcompare vivado/vivado_proj/vivado_proj.srcs/sources_1/bd/system/hw_handoff/system_bd.tcl vivado/bd/system.tcl
+bcompare vivado/vivado_proj/vivado_proj.srcs/constrs_1/new/system.xdc vivado/xdc/system.xdc
+bcompare vitis/src/vitis_proj/src vitis/sdk_workspace/vitis_proj/src
