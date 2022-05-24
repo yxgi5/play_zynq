e0619a9b9e98289f7095bde63b2520b866c8b3bb
Andreas Zhang <denglitsch@gmail.com>
2022-05-24 11:21:34 +0800

    fix: check_ping.py

--- lwip/gui/check_ping.py
+++ lwip/gui/check_ping.py
@@ -5,27 +5,12 @@ import socket
 
 def check_ping(host_ip='192.168.1.10'):
     # hostname = "google.com"
-    try:
-        response = os.system("ping -c 1 " + host_ip)
-        status = True
-    except Exception as ret:
-        #print(ret)
+    errorStr = 'Unreachable'
+    p = os.popen("ping -c 2 -r " + host_ip)
+    line = p.read()
+    if errorStr in line:
         status = False
-    
-    '''
-    # and then check the response...
-    if response == 0:
-        status = True
     else:
-        status = False
-    '''
-    
-    '''
-    try:
-        socket.gethostbyaddr(host_ip)
         status = True
-    except socket.herror:
-        status = False
-    '''
-    
+      
     return status
