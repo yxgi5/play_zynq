fdc9711ea3c337d023b35102dc917e2a9102734e
Andreas Zhang <denglitsch@gmail.com>
2022-05-26 01:24:37 +0800

    fix

--- lwip/vitis/flash.sh
+++ lwip/vitis/flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd output
-source /opt/Xilinx/Vitis/2020.1/settings64.sh
+source /opt/Xilinx/Vitis/2020.2/settings64.sh
 #echo "program_flash -f app.bin -offset 0 -flash_type mt25ql128-spi-x1_x2_x4 -verify -cable type xilinx_tcf url TCP:127.0.0.1:3121"
 #program_flash -f app.bin -offset 0 -flash_type mt25ql128-spi-x1_x2_x4 -verify -cable type xilinx_tcf url TCP:127.0.0.1:3121
 
