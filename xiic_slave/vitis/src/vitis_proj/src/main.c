6a490bcdfe4fa94dbc1089937ba41b96233faae7
Andreas Zhang <denglitsch@gmail.com>
2022-05-30 22:09:05 +0800

    feat: xiic_slave: reg ops slv fifo more than 16

--- xiic_slave/vitis/src/vitis_proj/src/main.c
+++ xiic_slave/vitis/src/vitis_proj/src/main.c
@@ -6,8 +6,8 @@
 #include "xiic.h"
 
 // IIC DEVICES
-#define MST_IIC_BASE_ADDR    XPAR_IIC_0_BASEADDR
-#define SLV_IIC_BASE_ADDR    XPAR_IIC_1_BASEADDR
+#define MST_IIC_BASE_ADDR    XPAR_IIC_1_BASEADDR
+#define SLV_IIC_BASE_ADDR    XPAR_IIC_0_BASEADDR
 
 #define PL_SLV_IIC_ITS_ADDR 0x48 // 8-bit address
 //#define PS_SLV_IIC_ITS_ADDR 0x8a
@@ -35,111 +35,15 @@ int main(void)
 //        for(int x = 0; x < 0x1000000; x++);
     }
 
-    //Master
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x100) = 0x1;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x120) = 0xF;
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x100 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x005;
-
-    while(((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x80)));
-
-//    usleep(500); // delay 500us @ 100kHz, then read SLV_IIC_BASE_ADDR + 0x10C
-//    pl_slv_read = *(u32 *) (SLV_IIC_BASE_ADDR + 0x10C);
-//    xil_printf("Slave IIC read value 0x%08x\n\r", pl_slv_read);
-//
-//    while(!(pl_slv_read == 0x5))
-//    {
-//        pl_slv_read = *(u32 *) (SLV_IIC_BASE_ADDR + 0x10C);
-//        xil_printf("Slave IIC read value 0x%08x\n\r", pl_slv_read);
-//    }
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x101 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x205;
-
-    usleep(1000);// at least 500us @ 100kHz, then read (MST_IIC_BASE_ADDR + 0x104)
-
-    pl_slv_read = *(u32 *) (SLV_IIC_BASE_ADDR + 0x10C);
-    xil_printf("Slave IIC read value 0x%08x\n\r", pl_slv_read);
-
-    for(i = 0; i < TEST_BUFFER_SIZE; i++)
+    while(1)
     {
-        while(!((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x40))) // // Check for RX_FIFO not empty to read data from RX_FIFO
+        if(((*(u32 *) (SLV_IIC_BASE_ADDR + 0x104)) & (0x80)))
         {
-            xil_printf("PL IIC read value 0x%08x\n\r", *(u32 *) (MST_IIC_BASE_ADDR + 0x10C));
-        }
-    }
-
-    /////////////////////////////////
-    sleep(1);
-
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x110) = PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x100) = 0x1;
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x120) = 0xF;
-    for(Index = 0x00; Index < 5; Index++)
-    {
-        *(u32 *) (SLV_IIC_BASE_ADDR + 0x108) = SendBuffer[Index]+1;
-        usleep(5000);
-//        for(int x = 0; x < 0x1000000; x++);
-    }
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x100) = 0x1;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x120) = 0xF;
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x100 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x005;
-
-    while(((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x80)));
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x101 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x205;
-
-    usleep(1000);// at least 500us @ 100kHz, then read (MST_IIC_BASE_ADDR + 0x104)
-
-    pl_slv_read = *(u32 *) (SLV_IIC_BASE_ADDR + 0x10C);
-    xil_printf("Slave IIC read value 0x%08x\n\r", pl_slv_read);
-
-    for(i = 0; i < TEST_BUFFER_SIZE; i++)
-    {
-        while(!((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x40))) // // Check for RX_FIFO not empty to read data from RX_FIFO
-        {
-            xil_printf("PL IIC read value 0x%08x\n\r", *(u32 *) (MST_IIC_BASE_ADDR + 0x10C));
-        }
-    }
-
-    /////////////////////////////////
-    sleep(1);
-
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x110) = PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x100) = 0x1;
-    *(u32 *) (SLV_IIC_BASE_ADDR + 0x120) = 0xF;
-    for(Index = 0x00; Index < 5; Index++)
-    {
-        *(u32 *) (SLV_IIC_BASE_ADDR + 0x108) = SendBuffer[Index]+2;
-        usleep(5000);
-//        for(int x = 0; x < 0x1000000; x++);
-    }
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x100) = 0x1;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x120) = 0xF;
-
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x100 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x005;
-
-    while(((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x80)));
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x101 | PL_SLV_IIC_ITS_ADDR;
-    *(u32 *) (MST_IIC_BASE_ADDR + 0x108) = 0x205;
-
-    usleep(1000);// at least 500us @ 100kHz, then read (MST_IIC_BASE_ADDR + 0x104)
-
-    pl_slv_read = *(u32 *) (SLV_IIC_BASE_ADDR + 0x10C);
-    xil_printf("Slave IIC read value 0x%08x\n\r", pl_slv_read);
-
-    for(i = 0; i < TEST_BUFFER_SIZE; i++)
-    {
-        while(!((*(u32 *) (MST_IIC_BASE_ADDR + 0x104)) & (0x40))) // // Check for RX_FIFO not empty to read data from RX_FIFO
-        {
-//            pl_mst_read[i] = *(u32 *)(MST_IIC_BASE_ADDR + 0x10C);  // Read data from RX FIFO
-            xil_printf("PL IIC read value 0x%08x\n\r", *(u32 *) (MST_IIC_BASE_ADDR + 0x10C));
+            for(Index = 0x00; Index < 5; Index++) // max 16 in fifo(0~15), overlaps when > 16, eg. 21, then fifo actual stores 5 bytes
+            {
+                *(u32 *) (SLV_IIC_BASE_ADDR + 0x108) = SendBuffer[Index];
+//                usleep(50);
+            }
         }
     }
 
