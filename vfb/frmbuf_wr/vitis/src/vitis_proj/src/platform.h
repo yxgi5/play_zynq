211fb4fd250530c0a6382a5170bffad4f3c0126e
Andreas Zhang <denglitsch@gmail.com>
2022-05-31 19:41:50 +0800

    feat: vfb/frmbuf_wr: xv_frmbufwr_example ok

---
+++ vfb/frmbuf_wr/vitis/src/vitis_proj/src/platform.h
@@ -0,0 +1,13 @@
+/******************************************************************************
+* Copyright (C) 2008 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+#ifndef __PLATFORM_H_
+#define __PLATFORM_H_
+
+void init_platform();
+void cleanup_platform();
+
+#endif
---
+++ vfb/frmbuf_wr/vivado/xdc/system.xdc
@@ -0,0 +1,4 @@
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
+
+set_property -dict {PACKAGE_PIN A17 IOSTANDARD LVCMOS33} [get_ports reset_n]
+
