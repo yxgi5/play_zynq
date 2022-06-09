e702ae01ce0901e019860cdfec43938e1c101447
Andreas Zhang <denglitsch@gmail.com>
2022-06-09 14:58:20 +0800

    update: script ver

--- lwip/vivado/setenv.sh
+++ lwip/vivado/setenv.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+echo -e "\033[42;31mScript ver 0.1.001 \nMake sure the env path is correct before running!! \033[0m"
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "OSTYPE = $OSTYPE"
+    #echo "OSTYPE = $OSTYPE"
     source /opt/Xilinx/Vivado/2020.2/settings64.sh
 else
-    echo "OSTYPE = $OSTYPE"
+	# tested with msys2
+    #echo "OSTYPE = $OSTYPE"
     source 'D:\Xilinx\Vivado\2020.2\settings64.sh'
 fi
 #vivado -mode tcl
