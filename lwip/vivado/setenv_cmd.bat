28a305f273eeb8d43f81d3e6d5c7e2e886694dd0
Andreas Zhang <denglitsch@gmail.com>
2022-06-14 18:26:15 +0800

    update: fix

--- lwip/vivado/setenv_cmd.bat
+++ lwip/vivado/setenv_cmd.bat
@@ -3,5 +3,5 @@ REM #################################################################
 REM # Copyright (c) 1986-2022 Xilinx, Inc.  All rights reserved.    #
 REM #################################################################
 
-CALL D:\Vivado\Vivado\2020.2\settings64.bat
+CALL D:\Xilinx\Vivado\2020.2\settings64.bat
 start cmd
