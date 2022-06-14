81893d8c1365c7793855fd4d863cd985740be4d5
Andreas Zhang <denglitsch@gmail.com>
2022-06-14 18:11:21 +0800

    update: scripts

---
+++ lwip/vivado/setenv_cmd.bat
@@ -0,0 +1,7 @@
+@echo off
+REM #################################################################
+REM # Copyright (c) 1986-2022 Xilinx, Inc.  All rights reserved.    #
+REM #################################################################
+
+CALL D:\Vivado\Vivado\2020.2\settings64.bat
+start cmd
